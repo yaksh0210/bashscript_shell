56eadedcd48c5d05737fc90388194ff72cf8a5f4
Yaksh Rawal <166778073+yaksh0210@users.noreply.github.com>
2024-04-12 12:39:02 +0530

    Add files via upload

---
+++ debugging/ex1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -evx
+
+#Running Command 1
+ls /home
+#Running Command 2
+ls /xyzw
+#Running Command 3
+ls /etc/passwd
---
+++ debugging/ex2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -vx
+
+#Running Command 1
+ls /home
+
+#Running Command 2
+ls /xyzw
+
+#Running Command 3
+ls /etc/passwd
